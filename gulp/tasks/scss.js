bf9fceb499b87e954cc97e533171cee8f5ae5d44
Eugene Korobkin <eugene_korobkin@mail.ru>
2022-10-03 23:08:18 +0300

    feat: add css plugins

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -1,6 +1,10 @@
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass'
 import rename from 'gulp-rename'
+import cleanCss from 'gulp-clean-css'; // Сжатие CSS файла
+import webpcss from 'gulp-webpcss'; // Вывод WEBP изображений
+import autoprefixer from 'gulp-autoprefixer'; // Добавление вендорных префиксов
+import groupCssMediaQueries from 'gulp-group-css-media-queries'; // Группировка медиа запросов
 
 const sass = gulpSass(dartSass); // Передача компилятора плагину gulpSass
 
@@ -16,6 +20,19 @@ export const scss = () => {
     .pipe(sass({
       outputStyle: 'expanded' // Стиль собранного файла
     }))
+    .pipe(groupCssMediaQueries())
+    .pipe(webpcss ({
+      webpClass: '.webp',
+      noWebpClass: '.no-webp',
+    }))
+    .pipe(autoprefixer({
+      grid: true,
+      overrideBrowserslist: ['last 3 versions'],
+      cascade: true,
+    }))
+    // Раскомментировать если нужна несжатая копия файла стилей
+    // .pipe(app.gulp.dest(app.path.build.css))
+    .pipe(cleanCss())
     .pipe(rename({
       extname: '.min.css'
     }))
