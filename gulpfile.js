6a54718e371129f1e4444e77e831fa2d80f86f87
Eugene Korobkin <eugene_korobkin@mail.ru>
2022-10-06 21:19:14 +0300

    feat: add ftp

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ import { images } from './gulp/tasks/images.js';
 import { otfToTtf, ttfToWoff, fontsStyle } from './gulp/tasks/fonts.js';
 import { svgSprive } from './gulp/tasks/svgSprive.js';
 import { zip } from './gulp/tasks/zip.js';
+import { ftp } from './gulp/tasks/ftp.js';
 
 //Передаем значения в глобальную переменную
 global.app = {
@@ -28,11 +29,14 @@ global.app = {
 
 //Наблюдатель за изменениями в файлах
 function watcher() {
-  gulp.watch(path.watch.files, copy)
-  gulp.watch(path.watch.html, html)
-  gulp.watch(path.watch.scss, scss)
-  gulp.watch(path.watch.js, js)
-  gulp.watch(path.watch.images, images)
+  // Если необходимо отслеживать изменения в файлах и автоматически обновлять их на сервере,
+  // то необходимо, чтобы нижеследующие записи были в подобном виде:
+  // gulp.watch(path.watch.html, gulp.series(html, ftp));
+  gulp.watch(path.watch.files, copy);
+  gulp.watch(path.watch.html, html);
+  gulp.watch(path.watch.scss, scss);
+  gulp.watch(path.watch.js, js);
+  gulp.watch(path.watch.images, images);
 }
 
 export {svgSprive}
@@ -48,11 +52,13 @@ const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images)
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server)); //series выполняет последовантельно вписанные сценарии
 const build = gulp.series(reset, mainTasks);
 const deployZIP = gulp.series(reset, mainTasks, zip);
+const deployFTP = gulp.series(reset, mainTasks, ftp);
 
 //Экспорт сценариев
 export { dev }
 export { build }
 export { deployZIP }
+export { deployFTP }
 
 //Выполнение сценария по умолчанию
 gulp.task('default', dev);
